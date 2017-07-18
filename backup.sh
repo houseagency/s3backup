cb9d27b0a92967fd5907c36550567603ce3b1325
Alfred Godoy <alfred@nurd.nu>
2017-07-18 09:50:57 +0200

    make s3 requests to the proper region

--- backup.sh
+++ backup.sh
@@ -9,7 +9,11 @@ for CREDENTIALS in $(cat credentials); do
     echo -e "\n==[ $BUCKET ]=================================="
     mkdir -p "data/$BUCKET"
     pushd "data/$BUCKET" >/dev/null
-    AWS_ACCESS_KEY_ID="$KEYID" AWS_SECRET_ACCESS_KEY="$SECRETKEY" aws s3 sync "s3://$BUCKET/" .
+    REGION="$(AWS_ACCESS_KEY_ID="$KEYID" AWS_SECRET_ACCESS_KEY="$SECRETKEY" aws s3api get-bucket-location --bucket "$BUCKET" | grep LocationConstraint | grep -v null | sed 's/.*: "//' | sed 's/"$//')"
+    if [ "$REGION" = "" ]; then
+      REGION="us-east-1"
+    fi
+    AWS_ACCESS_KEY_ID="$KEYID" AWS_SECRET_ACCESS_KEY="$SECRETKEY" AWS_DEFAULT_REGION="$REGION" aws s3 sync "s3://$BUCKET/" .
     popd >/dev/null
   done
 done
